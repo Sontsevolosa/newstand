8b1c8215c99f549c6f79eb5c614d1bf6717dbacd
codecademy <ccuser@codecademy.com>
2016-03-14 14:51:58 -0400

    Deploying to Heroku

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,8 @@
 class ArticlesController < ApplicationController
   before_action :require_user, only: [:index, :show]
   
+  before_action :require_author, only: [:edit, :update, :destroy]
+  
   def index
     @articles = Article.all
   end
